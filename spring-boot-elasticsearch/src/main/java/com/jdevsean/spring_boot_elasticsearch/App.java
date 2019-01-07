db5aeca5dfec8cd48634b775091f7ea58a420173
sean <devsean@foxmail.com>
2019-01-08 00:07:59 +0800

    提交-es初始化项目

---
+++ spring-boot-elasticsearch/src/main/java/com/jdevsean/spring_boot_elasticsearch/App.java
@@ -0,0 +1,13 @@
+package com.jdevsean.spring_boot_elasticsearch;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
